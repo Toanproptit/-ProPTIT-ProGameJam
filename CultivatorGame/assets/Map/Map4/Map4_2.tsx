7f44b72b0fabd3e252d3b04a773221cd00efdf9a
Toanproptit <nguyentrongtoana1byt@gmail.com>
2025-05-26 01:36:43 +0700

    done

--- CultivatorGame/assets/Map/Map4/Map4_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Map4_2" tilewidth="8" tileheight="8" tilecount="16384" columns="128">
- <image source="Map4_2.png" width="1024" height="1024"/>
-</tileset>
